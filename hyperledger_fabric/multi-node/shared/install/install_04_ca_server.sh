b62a3d418cd9340468ee62bc65072d2a3541db20
Heverson B. Ribeiro <heverson.ribeiro@b-com.com>
2019-07-09 10:51:25 +0200

    update: move ref doc to docs directory

--- hyperledger_fabric/multi-node/shared/install/install_04_ca_server.sh
+++ hyperledger_fabric/multi-node/shared/install/install_04_ca_server.sh
@@ -12,7 +12,6 @@ echo "--------------------------------------------------------------------------
 sudo apt install -y libtool libltdl-dev
 
 if [ $HOME = "/root" ]; then
-    echo "igual"
     export HOME="/home/$FABRIC_USER"
     echo "USER: $USER whoami: `whoami` id -un: `id -un` FABRIC_USER: $FABRIC_USER  HOME: $HOME  LOGNAME: $LOGNAME" 
     go get -u github.com/hyperledger/fabric-ca/cmd/...
@@ -20,7 +19,6 @@ if [ $HOME = "/root" ]; then
     sudo cp $GOPATH/bin/*  $HYPERLEDGER_HOME/bin
     export HOME="/root"
 else
-    echo "different"
     go get -u github.com/hyperledger/fabric-ca/cmd/...
     sudo cp $GOPATH/bin/*  /usr/local/bin
     sudo cp $GOPATH/bin/*  $HYPERLEDGER_HOME/bin
@@ -31,17 +29,8 @@ echo "USER: $USER whoami: `whoami` id -un: `id -un` FABRIC_USER: $FABRIC_USER  H
 ## Document process leads to errors as it leads to pulling of master branch
 # go get -u github.com/hyperledger/fabric-ca/cmd/...
 # go get -u https://github.com/hyperledger/fabric-ca/tree/release-1.4/cmd
-
-# git clone --branch release-1.3 https://github.com/hyperledger/fabric-ca.git
-# rm -rf $GOPATH/src/github.com/hyperledger/fabric-ca 2> /dev/null
-# mv fabric-ca  $GOPATH/src/github.com/hyperledger
-# go install github.com/hyperledger/fabric-ca/cmd/...
-
-#sudo cp $GOPATH/bin/*    /usr/local/bin
-
 # (original) sudo cp $GOPATH/bin/*    $PWD/../bin
-#sudo cp $GOPATH/bin/*  $HYPERLEDGER_HOME/bin
-
+# sudo cp $GOPATH/bin/*  $HYPERLEDGER_HOME/bin
 ### sudo rm $GOPATH/bin/* 
 
 echo "CA-server Done."
