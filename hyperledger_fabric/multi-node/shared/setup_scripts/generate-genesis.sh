523dbe3340a2144fa8c617fcb2c1bf8291b938bf
Heverson B. Ribeiro <heverson.ribeiro@b-com.com>
2019-08-02 16:44:28 +0200

    update

--- hyperledger_fabric/multi-node/shared/setup_scripts/generate-genesis.sh
+++ hyperledger_fabric/multi-node/shared/setup_scripts/generate-genesis.sh
@@ -3,10 +3,14 @@
 # export FABRIC_LOGGING_SPEC=INFO
 # export FABRIC_CFG_PATH=$PWD
 
+PROFILE=AirlineOrdererGenesis
+OUTBLOCK=./airline-genesis.block
+CHANNELID=ordererchannel
+
 # Create the Genesis Block
 echo    '================ Writing the Genesis Block ================'
 
-configtxgen -profile AirlineOrdererGenesis -outputBlock ./airline-genesis.block -channelID ordererchannel
+configtxgen -profile $PROFILE -outputBlock $OUTBLOCK -channelID $CHANNELID
 # profile : the profile from configtx.yaml
 # outputBlock : the path to write the genesis block
 # channelID  :  channel ID to use in the configtx
